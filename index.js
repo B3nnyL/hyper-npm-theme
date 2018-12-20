37f28c468df125c8a715ea39e4eaaa9915bb3b5d
B3nnyL <shuyinglin47@gmail.com>
2018-12-21 00:46:57 +1100

    [feat] tabs and active tab colored

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const backgroundColor = '#f3f3f3'
 const foregroundColor = '#6E7278'
 const cursorColor = '#6E7278'
-const borderColor = 'transparent'
+const borderColor = '#f3f3f3'
 
 const colors = {
   lightBlack: '#6E7278',
@@ -47,18 +47,45 @@ exports.decorateConfig = config => {
         background-color: #f3f3f3;
         color: ${foregroundColor};
       }
-      .tab_tab {
-        color: ${foregroundColor};
+      .tabs_title{
+        color: ${colors.yellow};
+      }
+      .tab_tab:nth-child(4n+1) {
+        color: ${colors.yellow};
+      }
+      .tab_tab:nth-child(4n+2) {
+        color: ${colors.red};
+      }
+      .tab_tab:nth-child(4n+3) {
+        color: ${colors.magenta};
+      }
+      .tab_tab:nth-child(4n+4) {
+        color: ${colors.blue};
+      }
+      .tab_tab:nth-child(4n+1).tab_active {
+        color: ${colors.yellow};
+        background-color:#FFF5DB;
+        border-bottom: 2px solid ${colors.yellow} !important;
+      }
+      .tab_tab:nth-child(4n+2).tab_active{
+        font-weight: 700;
+        color: ${colors.red};
+        background-color: #fdd8d6;
+        border-bottom: 2px solid ${colors.lightRed} !important;
+      }
+      .tab_tab:nth-child(4n+3).tab_active {
+        font-weight: 700;
+        color: ${colors.magenta};
+        background-color: #F6D8F4;
+        border-bottom: 2px solid ${colors.lightMagenta} !important;
+      }
+      .tab_tab:nth-child(4n+4).tab_active {
+        color: ${colors.blue};
+        background-color: #E8DEFF
+        border-bottom: 2px solid ${colors.lightBlue} !important;
       }
       .tab_tab:before {
       	border: 0;
-      }
-      .tab_tab.tab_active {
-        border: transparent;
-        font-weight: bold;
-        color: ${colors.red};
-        background - color: #fdd8d6;
-        border: ${colors.red}
       }
       .splitpane_divider {
       	background-color: ${colors.red};

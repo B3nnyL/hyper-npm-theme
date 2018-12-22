bfbf5e253a124cffd6444e79f948133adc8e7322
B3nnyL <shuyinglin47@gmail.com>
2018-12-22 18:55:18 +1100

    [fix] colors for better accessibility

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ const cursorColor = '#6E7278'
 const borderColor = '#f3f3f3'
 
 const colors = {
-  lightBlack: '#6E7278',
-  black: '#333333',
-  lightGreen: '#00C642',
-  green: '#00611F',
-  lightYellow: '#FFCD3A',
-  yellow: '#9E7800',
-  white: '#eee8d5',
-  lightWhite: '#fdf6e3',
-  lightRed: '#CB3837',
-  red: '#CB3837',
-  magenta: '#782176',
-  lightMagenta: '#C836C3',
-  lightBlue: '#8A56FF',
-  blue: '#28008A',
-  lightCyan: '#2AA198',
-  cyan: '#155651'
-
-}
+	lightBlack: "#6E7278",
+	black: "#333333",
+	lightGreen: "#57D57F",
+	green: "#00C642",
+	lightYellow: "#FFCD3A",
+	yellow: "#9E7800",
+	white: "#eee8d5",
+	lightWhite: "#fdf6e3",
+	lightRed: "#CB3837",
+	red: "#CB3837",
+	magenta: "#782176",
+	lightMagenta: "#C836C3",
+	lightBlue: "#8A56FF",
+	blue: "#28008A",
+	lightCyan: "#2AA198",
+	cyan: "#2AA198"
+};
 
 exports.decorateConfig = config => {
 
